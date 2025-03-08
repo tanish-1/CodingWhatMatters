4bf422a263a3fef539e11729a1fc095258e7239d
Tanish <imtanishlamba@gmail.com>
2025-03-08 19:07:43 +0530

    changes are saved

---
+++ Day13/ReactProjects/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+
+ const App = () => {
+ 
+  return (
+    <div>
+      <Header />
+      <Home/>
+      <Footer/>
+      </div>
+  )
+}
+
+export default App;
