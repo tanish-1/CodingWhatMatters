9db020ae52f9193e35732ec1b4c7e6c8f5987d43
tanish-1 <imtanishlamba.com>
2025-03-09 00:00:00 +0530

    changes are applied

--- Day14/ReactProjects/src/main.jsx
+++ Day14/ReactProjects/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from "./ThemeContext";
+
 
 import { register } from './serviceWorkerRegistration';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <ThemeProvider>
+
     <App />
-  </ThemeProvider>,
+  
   </StrictMode>,
 )
 
